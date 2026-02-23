ef77c540edba444425e537b990559bb5588adec2
Jonathon Penix <jpenix@qti.qualcomm.com>
2026-02-23 15:21:34 -0800

    [cpullvm] Add SWIG to our Linux dependencies for lldb
    Try adding for Windows through choco too just for grins
    
    Signed-off-by: Jonathon Penix <jpenix@qti.qualcomm.com>

--- qualcomm-software/scripts/install_dependencies.ps1
+++ qualcomm-software/scripts/install_dependencies.ps1
@@ -1,6 +1,9 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+# Try installing swig for lldb through choco just to see what happens.
+choco install swig
+
 # Install pyyaml
 pip install pyyaml
 
